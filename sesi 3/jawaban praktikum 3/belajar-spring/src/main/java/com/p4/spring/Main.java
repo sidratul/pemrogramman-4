b3d0dcbe000d9ee30e9f07cfeb8b595a6300d955
Muhammad Sidratul Muntaha <tear_ofregret@yahoo.co.id>
2013-04-26 22:28:37 +0700

    sesi 4 selesai

--- sesi 3/jawaban praktikum 3/belajar-spring/src/main/java/com/p4/spring/Main.java
+++ sesi 3/jawaban praktikum 3/belajar-spring/src/main/java/com/p4/spring/Main.java
@@ -2,17 +2,20 @@ package com.p4.spring;
 
 import com.p4.spring.Produk;
 import com.p4.spring.ProdukDao;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.ApplicationContext;
 import java.math.BigDecimal;
 
+import java.util.List;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
+
 public class Main {
-    public static void main(){
+    public static void main(String[] args){
         ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"config-spring.xml"});
         Produk p=context.getBean("p",com.p4.spring.Produk.class);
-        ProdukDao pd=context.getBean("pd",com.p4.spring.ProdukDao.class);
+        ProdukDao pd=context.getBean("pd",com.p4.spring.ProdukDao.class); 
         
-        
-        
+        pd.simpan(p);
+        List<Produk> lp = pd.cariSemuaProduk();
+       
     }
 }
--- sesi 3/jawaban praktikum 3/belajar-spring/src/main/java/com/p4/spring/ProdukDao.java
+++ sesi 3/jawaban praktikum 3/belajar-spring/src/main/java/com/p4/spring/ProdukDao.java
@@ -1,24 +1,26 @@
 package com.p4.spring;
 
-import com.p4.spring.Produk;
-import com.p4.spring.ProdukMapper;
+
+import com.mysql.jdbc.PreparedStatement;
+import java.util.ArrayList;
 import java.util.List;
 import javax.sql.DataSource;
-import org.springframework.jdbc.core.JdbcTemplate;
+
+//import org.springframework.jdbc.core.JdbcTemplate;
 
 public class ProdukDao {    
-    private DataSource ds;
-    private JdbcTemplate jt;
+    private DataSource ds;    
+    private PreparedStatement psInsert;
+    private PreparedStatement psUpdate;
+    private PreparedStatement psTampil;
+    
         
     public void simpan(Produk p){        
-        String SQL = "insert into Student (id,kode,nama,harga,terakhir_update) values (?,?,?,?)";
-        jt.update( SQL,p.getKode(),p.getNama(),p.getHarga(),p.getLastUpdate());
-        System.out.println("Created Record");       
+        
     }
 
-    public List<Produk> cariSemuaProduk(){
-        String SQL = "select * from produk";
-        List <Produk> produk = jt.query(SQL,new ProdukMapper());      
+    public List<Produk> cariSemuaProduk(){        
+        List <Produk> produk = new ArrayList<Produk>();
         return produk;
     }
 }
